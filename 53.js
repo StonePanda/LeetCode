5bd77ef345e82db29650004d06745f36db0acd9b
StonePanda <2504271266@qq.com>
2021-10-26 21:11:37 +0800

    211026动规

--- 53.js
+++ 53.js
@@ -44,14 +44,73 @@
  * @param {number[]} nums
  * @return {number}
  */
- var maxSubArray = function(nums) {
-    let maxsum=nums[0]
-    let i=sum=0
-    while(i<nums.length){
-        sum+=nums[i]
-        maxsum=(sum>maxsum)?sum:maxsum //这样可以让代码看起来更简洁
-        sum=(sum<=0)?0:sum
+var maxSubArray = function(nums) {
+    let maxsum = nums[0]
+    let i = sum = 0
+    while (i < nums.length) {
+        sum += nums[i]
+        maxsum = (sum > maxsum) ? sum : maxsum //这样可以让代码看起来更简洁
+        sum = (sum <= 0) ? 0 : sum
         i++
     }
     return maxsum
+};
+
+// 这道题自己做了一遍，然后初始化没有考虑好，导致提交了好几遍都是解答错误！
+// 但最后彻底不行的方式还是内存超出了：
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// var maxSubArray = function(nums) {
+//     // 重新再用动态规划的方式做一遍
+//     // 一维dp数组感觉不太合理，二维的话dp[i][j]就必须要满足一个i<=j
+//     if (nums.length == 1) {
+//         return nums[0]
+//     }
+//     let dp = Array(nums.length)
+//     for (let i = 0; i < dp.length; i++) {
+//         dp[i] = Array(nums.length).fill(0)
+//     }
+//     // 初始化没有初始化好！
+//     let result = nums[0]
+//     for (let i = 0; i < dp.length; i++) {
+//         for (let j = i; j < dp[0].length; j++) {
+//             if (j == 0) {
+//                 dp[i][j] = nums[0]
+//             } else {
+//                 dp[i][j] = dp[i][j - 1] + nums[j]
+//             }
+//             if (dp[i][j] > result) {
+//                 result = dp[i][j]
+//             }
+//         }
+//     }
+//     console.log(dp)
+//     return result
+// };
+
+
+// 我真是好蠢！！
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    // 重新再用动态规划的方式做一遍
+    // 天呐！我的脑子真是太笨啦！反正就是跟之前的二维dp数组缠上了！
+    // 就是用一维dp数组就可以！哎呀！！烦烦烦！！
+    let dp = Array(nums.length)
+        // 其实这道题的贪心思路就是，连续和为负我就舍弃之前的连续和，从现在开始往后面算
+        // dp[i]=Math.max(dp[i-1]+nums[i],nums[i])
+    dp[0] = nums[0]
+    let result = nums[0]
+    for (let i = 1; i < nums.length; i++) {
+        dp[i] = Math.max(nums[i], dp[i - 1] + nums[i])
+        if (dp[i] > result) {
+            result = dp[i]
+        }
+    }
+    return result
 };
