85943b39c698723d34f02e1678068e170dcba3b0
StonePanda <2504271266@qq.com>
2021-11-20 21:45:41 +0800

    211120链表

---
+++ 21.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+    // 做链表的题就是很容易晕晕！！
+    // 其实想到递归了！
+    // 但是在两个非空比较的时候使用两个tmp绕晕了
+    if (l1 == null) {
+        return l2
+    } else if (l2 == null) {
+        return l1
+    } else if (l1.val < l2.val) {
+        l1.next = mergeTwoLists(l1.next, l2)
+        return l1
+    } else {
+        l2.next = mergeTwoLists(l1, l2.next)
+        return l2
+    }
+};
